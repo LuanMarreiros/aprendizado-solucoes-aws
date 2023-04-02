7a2a3bb4abb8635c8a1da6f84692bb4735bc8402
LuanMarreiros <luan.marreiros@hotmail.com>
2023-04-02 17:01:43 -0300

    config jest

--- apps/mf-solucoes-aws/src/app/app.component.spec.ts
+++ apps/mf-solucoes-aws/src/app/app.component.spec.ts
@@ -1,32 +1,17 @@
-import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [AppComponent, NxWelcomeComponent],
-    }).compileComponents();
-  });
+  let component: AppComponent;
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+  beforeEach(() => {
+    component = new AppComponent();
+  })
 
-  it(`should have as title 'mf-solucoes-aws'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('mf-solucoes-aws');
-  });
+  it('deve ser iniciado', () => {
+    const spy = jest.spyOn(component, 'ngOnInit');
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome mf-solucoes-aws'
-    );
-  });
+    component.ngOnInit();
+
+    expect(spy).toBeCalled();
+  })
 });
